9d20e5425221ee9c079b9b86555c711119c4795a
Shahzad Ahmed <86219808+shahzad010@users.noreply.github.com>
2021-12-13 14:01:14 +0500

    Add files via upload
    test

---
+++ plus1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/home.Master" AutoEventWireup="true" CodeBehind="plus1.aspx.cs" Inherits="design.plus1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+        <br />
+        <asp:Image ID="Image2" runat="server" Height="80px" ImageUrl="~/images/plusCapture.PNG" />
+        <br />
+        <asp:PlaceHolder ID="PlaceHolder2" runat="server"></asp:PlaceHolder>
+        <br />
+        <asp:ImageButton ID="ImageButton2" runat="server" Height="90px" ImageUrl="~/images/equal.png" OnClick="ImageButton2_Click" />
+        <br />
+        <asp:PlaceHolder ID="PlaceHolder3" runat="server"></asp:PlaceHolder>
+        <br />
+        <asp:ImageButton ID="ImageButton3" runat="server" Height="50pt" ImageUrl="~/images/nextCapture.PNG" OnClick="ImageButton3_Click" />
+    </div>
+</asp:Content>
---
+++ plus1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace design
+{
+    public partial class plus1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(Request.QueryString["x"]);
+            int y = Convert.ToInt32(Request.QueryString["y"]);
+
+
+            for (int i = 1; i <= x; i++)
+                PlaceHolder1.Controls.Add(NewImage("images/apple.png"));
+
+            for (int i = 1; i <= y; i++)
+                PlaceHolder2.Controls.Add(NewImage("images/apple.png"));
+        }
+
+        public Image NewImage(string pathName)
+        {
+
+            Image img = new Image();
+            img.ImageUrl = pathName;
+            img.Width = 100;
+            img.Height = 100;
+            return img;
+        }
+
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+            int x = Convert.ToInt32(Request.QueryString["x"]);
+            int y = Convert.ToInt32(Request.QueryString["y"]);
+            for (int i = 1; i <= x + y; i++)
+                PlaceHolder3.Controls.Add(NewImage("images/apple.png"));
+        }
+
+        protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("plus.aspx");
+        }
+    }
+}
